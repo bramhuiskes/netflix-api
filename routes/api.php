ab4028cb8350ab4203cc6b2949032229a22653e4
GADijkhuis <gideon.dijkhuis@student.nhlstenden.com>
2024-12-04 10:35:32 +0100

    Solved issue with API routes

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Openbare API-routes
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-
-// Beschermde API-routes (alleen toegankelijk na authenticatie)
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/', function () {
+    return response()->json(['message' => 'Test from API route']);
 });
+
+//// Openbare API-routes
+//Route::post('/register', [UserController::class, 'register']);
+//Route::post('/login', [UserController::class, 'login']);
+//
+//// Beschermde API-routes (alleen toegankelijk na authenticatie)
+//Route::middleware('auth:sanctum')->group(function () {
+//    Route::get('/user', function (Request $request) {
+//        return $request->user();
+//    });
+//
+//    Route::get('/movies', [MovieController::class, 'index']);
+//});
