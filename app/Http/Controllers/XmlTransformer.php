a7df0957c295de2571e76b32accf4c1be071660a
Gideon Dijkhuis <dijkhuis.g.a@outlook.com>
2025-01-07 17:45:13 +0100

    Added expire time for tokens for 1 day

--- app/Http/Controllers/XmlTransformer.php
+++ app/Http/Controllers/XmlTransformer.php
@@ -9,16 +9,12 @@ class XmlTransformer
         $xml = new \SimpleXMLElement("<{$rootElement}/>");
         array_walk_recursive($data, function($value, $key) use ($xml) {
             if (XmlTransformer::isJson($value)) {
-                // If the value is a JSON string, decode it
                 $decoded = json_decode($value, true);
-                // Create a new child node for the JSON object
                 $jsonNode = $xml->addChild($key);
-                // Add the JSON object's properties as child nodes
                 array_walk_recursive($decoded, function($jsonValue, $jsonKey) use ($jsonNode) {
                     $jsonNode->addChild($jsonKey, htmlspecialchars((string)$jsonValue));
                 });
             } else {
-                // Add the value as text if it's not JSON
                 $xml->addChild($key, htmlspecialchars((string)$value));
             }
         });
